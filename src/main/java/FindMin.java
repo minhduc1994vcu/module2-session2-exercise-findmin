96f82f5c5a1de3634b65835cb93da2e44f07cfbb
minhduc <minhduc1994vcu@gmail.com>
2019-03-27 19:20:21 +0700

    add final file

---
+++ src/main/java/FindMin.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+public class FindMin {
+    public static void main(String[] args) {
+        int[] array = {9, 34, 2, 1, 54, 3, 2};
+        int index = findMin(array);
+        System.out.printf("the min is %d at position %d", array[index], index);
+    }
+
+    public static int findMin(int[] arr) {
+        int min = arr[0];
+        int index = 0;
+        for (int i = 1; i < arr.length; i++) {
+            if (arr[i] < min) {
+                min = arr[i];
+                index = i;
+            }
+        }
+        return index;
+
+    }
+}
